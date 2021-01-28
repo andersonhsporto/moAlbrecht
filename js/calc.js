542911939034c9e2a4b3172d4f38d0637c715b9a
Anderson Porto <47704550+andersonhsporto@users.noreply.github.com>
2021-01-28 01:11:23 -0300

    Update calc.js

--- js/calc.js
+++ js/calc.js
@@ -4,10 +4,10 @@ $(function(){
         const mo_ideal = parseFloat($('#MO_ideal').val())
         const metros_ = mo_ideal - milhas +"%"
         const metros = metros_.toString().replace(".", ",");
-        const metros2 = mo_ideal + milhas +"%"
+        const metros2 = mo_ideal + milhas +" %"
         const ppmd_ = (mo_ideal - milhas) * 10000 + " ppm"
         const ppmd = ppmd_.toString().replace(".", ",");
-        const kghade_ = ((mo_ideal - milhas) * 2 * 10000) + " kg/ha"
+        const kghade_ = ((mo_ideal - milhas) * 2 * 10000) + " kg/ha de Matéria orgânica"
         const kghade = kghade_.toString().replace(".", ",");
         $('#metros').val(metros)
         $('#ppmd').val(ppmd)
