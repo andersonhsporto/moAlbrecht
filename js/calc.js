937914668c6a788d64704ed384964818c95f09bc
Anderson Porto <47704550+andersonhsporto@users.noreply.github.com>
2021-01-28 01:02:01 -0300

    Add files via upload

---
+++ js/calc.js
@@ -0,0 +1,18 @@
+$(function(){
+    $('#calc').click(function(){
+        const milhas = parseFloat($('#MO_analise').val())
+        const mo_ideal = parseFloat($('#MO_ideal').val())
+        const metros_ = mo_ideal - milhas +"%"
+        const metros = metros_.toString().replace(".", ",");
+        const metros2 = mo_ideal + milhas +"%"
+        const ppmd_ = (mo_ideal - milhas) * 10000 + " ppm"
+        const ppmd = ppmd_.toString().replace(".", ",");
+        const kghade_ = ((mo_ideal - milhas) * 2 * 10000) + " kg/ha"
+        const kghade = kghade_.toString().replace(".", ",");
+        $('#metros').val(metros)
+        $('#ppmd').val(ppmd)
+        $('#kghade').val(kghade)
+        
+    })
+})
+
