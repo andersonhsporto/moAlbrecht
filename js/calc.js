4b1fe32bb82c09f23997073348fbec55ac265ca4
Anderson Porto <47704550+andersonhsporto@users.noreply.github.com>
2021-01-28 15:55:30 -0300

    Unidades

--- js/calc.js
+++ js/calc.js
@@ -4,11 +4,10 @@ $(function(){
         const mo_ideal = parseFloat($('#MO_ideal').val());
         const metros_ = mo_ideal - milhas;
         const metros = metros_.toString().replace(".", ",") +" %";
-        const metros2 = mo_ideal + milhas +" %";
         const ppmd_ = (mo_ideal - milhas) * 10000 + " ppm"
-        const ppmd = ppmd_.toString().replace(".", ",");
+        const ppmd = ppmd_.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
         const kghade_ = ((mo_ideal - milhas) * 2 * 10000);
-        const kghade = kghade_.toString().replace(".", ",")+ " kg/ha de M.O.";
+        const kghade = kghade_.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")+ " kg/ha de M.O.";
         $('#metros').val(metros)
         $('#ppmd').val(ppmd)
         $('#kghade').val(kghade)
