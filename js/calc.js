4a6f08c06b263cedb262b4bc9fbeb67143dfa4a9
Anderson Porto <47704550+andersonhsporto@users.noreply.github.com>
2021-01-28 22:29:02 -0300

    Remoção do webkit

--- js/calc.js
+++ js/calc.js
@@ -1,14 +1,14 @@
 $(function(){
     $('#calc').click(function(){
-        const milhas = parseFloat($('#MO_analise').val());
+        const mo_analise = parseFloat($('#MO_analise').val());
         const mo_ideal = parseFloat($('#MO_ideal').val());
-        const metros_ = mo_ideal - milhas;
-        const metros = metros_.toString().replace(".", ",") +" %";
-        const ppmd_ = (mo_ideal - milhas) * 10000 + " ppm"
+        const defitt_ = mo_ideal - mo_analise;
+        const defitt = defitt_.toString().replace(".", ",") +" %";
+        const ppmd_ = (mo_ideal - mo_analise) * 10000 + " ppm"
         const ppmd = ppmd_.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.");
-        const kghade_ = ((mo_ideal - milhas) * 2 * 10000);
+        const kghade_ = ((mo_ideal - mo_analise) * 2 * 10000);
         const kghade = kghade_.toString().replace(/(\d)(?=(\d{3})+(?!\d))/g, "$1.")+ " kg/ha de M.O.";
-        $('#metros').val(metros)
+        $('#defitt').val(defitt)
         $('#ppmd').val(ppmd)
         $('#kghade').val(kghade)
         
